4d743aa0169c6b64a11df81e97f54b0311a21985
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-02-11 19:17:10 -0600

    stop timer when 0 is reached

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 // When the user clicks a button, the timer starts based on that #
+// timer display element
+var microwaveTimerEl = document.getElementById('timeDisplay');
 
 // start button -- use function format for # buttons 
 var startEl = document.getElementById('start');
 startEl.addEventListener('click', function(){
-	console.log("click");
+	console.log("start!");
     timeSelected();
     startEl.disabled = true;
     stopEl.removeAttribute('disabled');
@@ -21,47 +23,48 @@ stopEl.addEventListener('click', function(){
 // clear button
 var clearEl = document.getElementById('clear');
 clearEl.addEventListener('click', function(){
-    console.log("clear!");
     time = 0;
     timeSelected(time);
 })
 
-// right now, timer set to 10 minutes
-var timeAtStart = 10
+// timer variables
+var timeAtStart = .2
 let time = timeAtStart * 60;
 var beginMicrowave = -1;
-
-var microwaveTimerEl = document.getElementById('timeDisplay');
-
-
 
 function timeSelected () {
    if (time > 0) {
     if (beginMicrowave == -1) {
         beginMicrowave =  setInterval(function(){
-       // convert the time to mins/secconds
-       var mins = Math.floor(time / 60);
-       // add placeholder "0" for format
-       if (mins<10) {
-           mins = '0' + mins;
-       }
-       let secs = time % 60;
-       // add placeholder "0" for format
-       if (secs<10) {
-           secs = '0' + secs;
-       }
+            if (time > 0) {
+                // convert the time to mins/secconds
+                var mins = Math.floor(time / 60);
+                // add placeholder "0" for format
+                if (mins<10) {
+                    mins = '0' + mins;
+                }
+                let secs = time % 60;
+                // add placeholder "0" for format
+                if (secs<10) {
+                    secs = '0' + secs;
+                }
 
-       microwaveTimerEl.innerHTML = mins + ":" + secs;
-       time --;
+                microwaveTimerEl.innerHTML = mins + ":" + secs;
+                time --;
+            }
+            else {
+            console.log("ding");
+            clearInterval(beginMicrowave);
+            microwaveTimerEl.innerHTML = "00:00";
+            }
         }, 1000);
     }
     else {
         clearInterval(beginMicrowave);
-        console.log(beginMicrowave);
         beginMicrowave = -1;
     }
    }
-   else if (time ==  0) {
+   else if (time == 0) {
     // stop countdown
     clearInterval(beginMicrowave);
     // display time
